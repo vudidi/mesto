3b1f20bf71b0bf52d867bfd267581714449c26af
Vika Dovgal <vikadovgal@yandex.ru>
2021-12-29 05:41:33 +0400

    feat: добавлен новый функционал (лайки, просмотр, удаление, добавление)

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,129 @@
-const popup = document.querySelector('.popup');
-const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonClose = popup.querySelector('.popup__close');
-const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__form-input_info_name');
-const jobInput = popup.querySelector('.popup__form-input_info_about');
+// Переменные попапов
+const profileInfoPopup = document.querySelector('.popup_type_profile');
+const cardContentPopup = document.querySelector('.popup_type_card');
+const buttonEditProfile = document.querySelector('.profile__button-edit');
+const buttonAddCard = document.querySelector('.profile__button-add');
+
+// Переменные для попапа профиля
+const buttonCloseProfile = profileInfoPopup.querySelector('.popup__close');
+const formElementProfile = profileInfoPopup.querySelector('.popup__form');
+const nameInput = profileInfoPopup.querySelector('.popup__form-input_info_name');
+const jobInput = profileInfoPopup.querySelector('.popup__form-input_info_about');
 const nameProfile = document.querySelector('.profile__title');
 const jobProfile = document.querySelector('.profile__subtitle');
 
-function openPopup() {
+// Переменные для попапа карточек
+const buttonCloseCardContent = cardContentPopup.querySelector('.popup__close');
+const formElementCardContent = cardContentPopup.querySelector('.popup__form');
+const titleInput = cardContentPopup.querySelector('.popup__form-input_info_title');
+const linkInput = cardContentPopup.querySelector('.popup__form-input_info_link');
+
+// Переменные для попапа фотографии
+const popupImage = document.querySelector('.popup-photo');
+const buttonClosepopupImage = popupImage.querySelector('.popup__close');
+const photoTitle = popupImage.querySelector('.popup-photo__title');
+const photoLink = popupImage.querySelector('.popup-photo__link');
+
+// Открыть попап и Закрыть попап. Общие
+function openPopup(element) {
+  element.classList.add('popup_opened')
+};
+
+function closePopup(element) {
+  element.classList.remove('popup_opened')
+};
+
+// Открыть попап карточки
+function openPopupCardContent() {
+  cardTitle.value = "";
+  cardLink.value = "";
+  openPopup(cardContentPopup)
+}
+
+// Открыть попап профиля
+function openPopupProfileInfo() {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
-  popup.classList.add('popup_opened');
-}
+  openPopup(profileInfoPopup);
+};
 
-function closePopup() {
-  popup.classList.remove('popup_opened')  
-}
-
-function submitFormHandler(evt) {
+// Сохранить попап профиля
+function submitFormHandlerProfile(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  closePopup()
+  closePopup(profileInfoPopup)
+};
+
+// Слушатели
+buttonEditProfile.addEventListener('click', openPopupProfileInfo);
+buttonCloseProfile.addEventListener('click', () => closePopup(profileInfoPopup));
+buttonAddCard.addEventListener('click', openPopupCardContent);
+buttonCloseCardContent.addEventListener('click', () => closePopup(cardContentPopup));
+
+// Поставить лайк фото
+function likePhoto(e) {
+  e.target.classList.toggle('card__like_active')
+};
+
+// Удалить карточку
+function removeCard(e) {
+  e.target.closest('.card').remove()
+};
+
+// Открыть изображение
+function handleImageClick(title, link) {
+  photoTitle.textContent = title;
+  photoLink.src = link;
+  openPopup(popupImage)
 }
 
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
-formElement.addEventListener('submit', submitFormHandler);
+// Клонировать шаблон карточки
+const cardsTemplate = document.querySelector('#tmpl-cards').content;
+const cardList = document.querySelector('.cards');
+
+function cloneCardTemplate(item) {
+  const cardElement = cardsTemplate.cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+
+  cardElement.querySelector('.card__image').addEventListener('click', function () {
+    handleImageClick(item.name, item.link)
+  })
+  buttonClosepopupImage.addEventListener('click', () => closePopup(popupImage));
+
+  const likeCardButton = cardElement.querySelector('.card__like');
+  const deleteCardButton = cardElement.querySelector('.card__delete');
+
+  formElementProfile.addEventListener('submit', submitFormHandlerProfile);
+  formElementCardContent.addEventListener('submit', submitFormHandlerCard);
+  likeCardButton.addEventListener('click', likePhoto);
+  deleteCardButton.addEventListener('click', removeCard);
+
+  return cardElement
+};
+
+// Получить шаблон для карточки
+function receiveCard(item) {
+  const newCard = cloneCardTemplate(item);
+  cardList.prepend(newCard)
+};
+
+// Добавить массив карточек
+initialCards.forEach(getInitialCards => {
+  receiveCard(getInitialCards)
+});
+
+// Создать карточку
+const cardTitle = document.querySelector('.popup__form-input_info_title');
+const cardLink = document.querySelector('.popup__form-input_info_link');
+
+function submitFormHandlerCard(evt) {
+  evt.preventDefault();
+  receiveCard({
+    name: cardTitle.value,
+    link: cardLink.value
+  });
+  closePopup(cardContentPopup);
+};
---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+    name: 'Йеллоустонский национальный парк',
+    link: 'https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Чима д`Аста',
+    link: 'https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80'
+  },
+  {
+    name: 'Гейрангер-фьорд',
+    link: 'https://images.unsplash.com/photo-1497273145860-da6d5426776f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80'
+  },
+  {
+    name: 'Капитол-Риф',
+    link: 'https://images.unsplash.com/photo-1590377077710-f024869db419?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'Улуру',
+    link: 'https://images.unsplash.com/photo-1627868053217-b5cf77505bc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'Карапынар',
+    link: 'https://images.unsplash.com/photo-1631010098924-16f28a81ef8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1161&q=80'
+  }
+]
